d26650c35cbe7eef4e4ff9522cefc6b7bfa49bf4
AdamantLife <adamantlife@gmail.com>
2021-10-10 20:07:24 -0400

    Removed debugging
    Logging lines were left in

--- FunimationDates/dater_releases.js
+++ FunimationDates/dater_releases.js
@@ -3,17 +3,10 @@ const DATERE = /(?:(?<days>\d+)\s+days?\s*)?(?:(?<hours>\d+)\s+hours?\s*)?(?:(?<
 function addDates(){
     for( let ele of document.getElementsByClassName("new-releases-slide-date")){
         let text = ele.textContent;
-        console.log(text);
         let result = DATERE.exec(text);
-        console.log(">", result);
         if(!result || result === undefined) continue;
         let day = new Date();
-        console.log(`----------
-Minutes: ${result.groups.minutes}
-Hours: ${result.groups.hours}
-Days: ${result.groups.days}
-----------
-`)
+
         let moffset = result.groups.minutes | 0;
         let hoffset = result.groups.hours || 0;
         let doffset = result.groups.days || 0;
